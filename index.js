570da9e55a8edf046e8e70f99a1671a257a7f68f
Shiyeon Ku <c.monk@wustl.edu>
2024-02-03 20:45:39 -0600

    all information included
    inclusion of all of the elements necessary for the assignment

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-let modal = null;
-
 //grab the initial button then insert the modal
 document.addEventListener('click', function(e){
-    console.log(e.target);
-    modalOpen(e.target.id);
+    console.log(e.target.id);
+    action(e.target.id);
     //depending on the target open different modals
 })
 
-function modalOpen (vairable){
+//listen to when form opener is clicked
+
+function action (vairable){
     //grab modal and open it up
     if (vairable =="aic_overlay"){
         let modal = document.getElementById("m_aic");
@@ -27,6 +27,18 @@ function modalOpen (vairable){
     if(vairable == "hpac_overlay"){
         let modal = document.getElementById("m_aic");
         modal.style.display = "block";
+    }
+
+    //if it's a form button open form button
+    if(vairable == "form_btn"){
+        let form = document.getElementById("interest_form");
+        form.style.display = "block";
+    }
+
+    //if variable is close button close the display
+    if(vairable == "close_btn"){
+        let form = document.getElementById("interest_form");
+        form.style.display = "none";
     }
 }
 
